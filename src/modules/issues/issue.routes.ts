778fe998b1b50a888386f8f9646a0bfdfd2ac71e
Yousuf7900 <yousufislam7900@gmail.com>
2026-05-23 21:01:40 +0600

    This commit is useless

--- src/modules/issues/issue.routes.ts
+++ src/modules/issues/issue.routes.ts
@@ -10,6 +10,6 @@ router.post("/", verifyToken, createIssue);
 router.get("/", getAllIssues);
 router.get("/:id", getSingleIssue);
 router.patch("/:id", verifyToken, updateIssue);
-router.delete("/:id", verifyToken, deleteIssue)
+router.delete("/:id", verifyToken, deleteIssue);
 
 export const issueRoutes = router;
