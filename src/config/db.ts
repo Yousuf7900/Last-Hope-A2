e48e1ef1e57d03e3c76d0941a262eba4dcf672d4
Yousuf7900 <yousufislam7900@gmail.com>
2026-05-23 14:11:27 +0600

    PostgreSQL using NeonDB connected successfully with the pg

---
+++ src/config/db.ts
@@ -0,0 +1,24 @@
+import { Pool } from "pg";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("Database url is missing on env");
+}
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+export const connectDB = async (): Promise<void> => {
+    try {
+        const client = await pool.connect();
+        console.log("PostgreSQL connected successfully");
+        client.release();
+    } catch (error) {
+        console.error("PostgreSQL connection failed", error);
+        process.exit(1);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,16 @@ dotenv.config();
 
 import app from "./app";
 
+import { connectDB } from "./config/db";
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server running on ${port}`);
-})
+const startServer = async (): Promise<void> => {
+    await connectDB();
+
+    app.listen(port, () => {
+        console.log(`Server running on ${port}`);
+    })
+}
+
+startServer();
