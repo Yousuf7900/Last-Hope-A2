067423c9c5df8d3f2cac6ebcef5fe7ee2dd40a19
Yousuf7900 <yousufislam7900@gmail.com>
2026-05-23 17:48:30 +0600

    Now user can login successfully and some validation check

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import type { Request, Response } from "express";
 import sendResponse from "../../utils/sendResponse";
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
 import { pool } from "../../config/db";
 
+dotenv.config();
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
@@ -60,6 +63,67 @@ export const registerUser = async (req: Request, res: Response) => {
             success: false,
             statusCode: 500,
             message: "Registration failed",
+            errors: error
+        })
+    }
+};
+
+
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return sendResponse(res, {
+                success: false,
+                statusCode: 400,
+                message: "Email & Password required"
+            })
+        }
+
+        const result = await pool.query(
+            `
+            SELECT * FROM users WHERE email=$1
+            `, [email]
+        );
+
+        if (result.rows.length === 0) {
+            return sendResponse(res, {
+                success: false,
+                statusCode: 401,
+                message: "Invalid email or password"
+            });
+        }
+
+        const user = result.rows[0];
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatched) {
+            return sendResponse(res, {
+                success: false,
+                statusCode: 401,
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign({ id: user.id, name: user.name, role: user.role }, process.env.JWT_SECRET as string, { expiresIn: "1d" });
+
+        delete user.password;
+
+        return sendResponse(res, {
+            success: true,
+            statusCode: 200,
+            message: "Login successfull",
+            data: {
+                token, user,
+            }
+        })
+    } catch (error) {
+        return sendResponse(res, {
+            success: false,
+            statusCode: 500,
+            message: "Login failed",
             errors: error
         })
     }
--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { registerUser } from "./auth.controller";
+import { loginUser, registerUser } from "./auth.controller";
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
+router.post("/login", loginUser);
 
 
 export const authRoutes = router;
